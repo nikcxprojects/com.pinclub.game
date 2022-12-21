9a911db432c6d02ec723fd64b6e80667a37340af
Sladkiy Eduard <nikcxprojects@gmail.com>
2022-12-21 13:53:41 +0300

    update

--- Assets/com.pinclub.game/Scripts/Card Game/CardGameManager.cs
+++ Assets/com.pinclub.game/Scripts/Card Game/CardGameManager.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 using Random = UnityEngine.Random;
 
 public class CardGameManager : MonoBehaviour
 {
+    public static Action OnCardGet { get; set; }
+
     private void OnEnable()
     {
         StartGame();
@@ -12,19 +17,35 @@ public class CardGameManager : MonoBehaviour
 
     private void StartGame()
     {
-        Card[] cards = Resources.LoadAll<Card>("Cards");
-        for(int i = 0; i < cards.Length; i++)
+        List<Card> cards = Resources.LoadAll<Card>("Cards").ToList();
+        for(int i = 0; i < cards.Count; i++)
         {
             Card tmp = cards[i];
-            int rv = Random.Range(i, cards.Length);
+            int rv = Random.Range(i, cards.Count);
 
             cards[rv] = cards[i];
             cards[i] = tmp;
         }
 
-        for(int i = 0; i < cards.Length; i++)
+        StartCoroutine(DropCards(cards));
+    }
+
+    private IEnumerator DropCards(List<Card> cards)
+    {
+        float dropOffset = 0.25f;
+        Player[] players = FindObjectsOfType<Player>();
+
+        while(cards.Count > 0)
         {
-            Card card = Instantiate(cards[i], GameObject.Find("deck").transform);
+            for(int i = 0; i < players.Length; i++)
+            {
+                Card card = Instantiate(cards.Last(), GameObject.Find("deck").transform);
+                cards.Remove(card);
+
+                yield return new WaitForSeconds(dropOffset);
+            }
+
+            yield return null;
         }
     }
 }
--- Assets/com.pinclub.game/Scripts/Card Game/Player.cs
+++ Assets/com.pinclub.game/Scripts/Card Game/Player.cs
@@ -1,6 +1,19 @@
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
+    private int count;
+    private TextMeshPro countText;
+
     public bool IsBot;
+
+    private void Awake()
+    {
+        countText = GetComponent<TextMeshPro>();
+        CardGameManager.OnCardGet += () =>
+        {
+            countText.text = $"{++count}";
+        };
+    }
 }
